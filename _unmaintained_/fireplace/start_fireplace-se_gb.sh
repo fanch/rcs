f1bcd5ac06681ad3030db97bc1e3b53dcbed93b3
Ali H. Caliskan <ali.h.caliskan@gmail.com>
2017-03-18 08:52:54 +0100

    Update recipe

--- _unmaintained_/fireplace/start_fireplace-se_gb.sh
+++ _unmaintained_/fireplace/start_fireplace-se_gb.sh
@@ -2,8 +2,9 @@
 
 export TERMINAL=terminology
 
-export XKB_DEFAULT_LAYOUT="gb"
+export XKB_DEFAULT_LAYOUT="gb,se"
 export XKB_DEFAULT_VARIANT=",nodeadkeys"
 export XKB_DEFAULT_MODEL="pc105"
+export XKB_DEFAULT_OPTIONS="grp:alt_shift_toggle"
 
 fireplace
