2557394dc24ab79309fd1fe3fdb7139f4443715c
Ali H. Caliskan <ali.h.caliskan@gmail.com>
2017-03-18 08:46:46 +0100

    Update recipe

--- sway/sway-se_gb.sh
+++ sway/sway-se_gb.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export XKB_DEFAULT_LAYOUT="se"
+export XKB_DEFAULT_LAYOUT="se,gb"
 export XKB_DEFAULT_VARIANT=",nodeadkeys"
 export XKB_DEFAULT_MODEL="pc105"
+export XKB_DEFAULT_OPTIONS="grp:alt_shift_toggle"
 
 sway
